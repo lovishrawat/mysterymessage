5befef33577a9a9ef76cbb7966ce3b2a66a539a2
lovishrawat <156170290+lovishrawat@users.noreply.github.com>
2024-09-15 23:38:16 +0530

    SecretApp First

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from 'next/server';
+export { default } from "next-auth/middleware";
+import { getToken } from 'next-auth/jwt';
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+  const url = request.nextUrl;
+
+  if (token && (
+    url.pathname.startsWith('/sign-in') ||
+    url.pathname.startsWith('/sign-up') ||
+    url.pathname.startsWith('/verify')
+  )) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+
+  if (!token && url.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
+
+  return NextResponse.next();  // Allow the request to continue if no redirect is needed
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/sign-in',
+    '/sign-up',
+    '/',
+    '/dashboard/:path*',
+    '/verify/:path*'
+  ]
+};
